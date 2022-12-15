f1cd04958e6ee42942f8dca7f9d1b802b753bb74
victor <victor.buthia@student.moringaschool.com>
2022-12-15 22:10:40 +0300

    added

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ function showGithubUserProfile(){
     let url = 'https://api.github.com/users/'+username
     fetch(url).then(res=>res.json())
     .then(data=>{
-        console.log(data)
+        if(data.message){
+            console.log('User Profile Not Found')
+        }else{
+            console.log(data)
+        } 
+        
     }).catch(e=>{
         console.log(e)
     })
